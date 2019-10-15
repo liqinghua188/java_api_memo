b6c26c4212fd5c3cc87008fbbfcfa87f7d069d00
liqinghua <519938300@qq.com>
2019-10-15 09:05:30 +0800

    java8 Collector

---
+++ collector/src/main/java/com/example/collector/ToListCollector.java
@@ -0,0 +1,47 @@
+package com.example.collector;
+
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+
+public class ToListCollector<T> implements Collector<T, List<T>, List<T>> {
+    @Override
+    public Supplier<List<T>> supplier() {
+        return ArrayList::new;
+    }
+
+    @Override
+    public BiConsumer<List<T>, T> accumulator() {
+        return (container, t) -> container.add(t);
+    }
+
+    @Override
+    public BinaryOperator<List<T>> combiner() {
+        return (left, right) -> {
+            left.addAll(right);
+            return left;
+        };
+    }
+
+    @Override
+    public Function<List<T>, List<T>> finisher() {
+        return Function.identity();
+    }
+
+    @Override
+    public Set<Characteristics> characteristics() {
+        return Collections.unmodifiableSet(EnumSet.of(Characteristics.IDENTITY_FINISH, Characteristics.CONCURRENT));
+    }
+
+    public static void main(String[] args) {
+        List<String> strList = Arrays.asList("1", "2", "3", "4", "5");
+
+        List<Integer> nums = strList.stream().map(Integer::parseInt).collect(new ToListCollector<>());
+        Optional.ofNullable(nums).ifPresent(
+                System.out::println
+        );
+    }
+}
